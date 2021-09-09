0ecce276c52492d4e4bf8691f46d14b4c7ce7744
Behnam Mahdavirad <behnam.mahdavirad@gmail.com>
2021-09-09 16:43:55 +0430

    Add ConcreteDecorator class

---
+++ Decorator/ConcreteDecorator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class ConcreteDecorator : Decorator
+    {
+        public ConcreteDecorator(Component component) : base(component)
+        {
+
+        }
+
+        public override void Operation()
+        {
+            base.Operation();
+            NewFeature();
+        }
+
+        public void NewFeature()
+        {
+            Console.WriteLine("This is NewFeature()");
+        }
+    }
+}
---
+++ Decorator/Decorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public abstract class Decorator : Component
+    {
+        private readonly Component _component;
+
+        public Decorator(Component component)
+        {
+            _component = component;
+        }
+
+        public override void Operation()
+        {
+            _component.Operation();
+        }
+    }
+}
